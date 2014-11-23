52a911e39c789f2cc57d246be995dee3f7ed22c8
Taylor <tvance989@gmail.com>
2014-11-22 18:36:13 -0600

    server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express();
+var pg = require('pg');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        console.log(process.env.DATABASE_URL);
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err) {
+                console.error(err);
+                response.send("Error " + err);
+            } else {
+                response.send(result.rows);
+            }
+        });
+    });
+});
+
+app.get('/', function(request, response) {
+    app.listen(app.get('port'), function() {
+        console.log("Node app is running at localhost:" + app.get('port'))
+    });
